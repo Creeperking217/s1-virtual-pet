ad8b87cb0740735a594191efe0b3f4d97ba3b865
Your Name <you@example.com>
2023-10-16 14:51:30 -0700

    as;klfasdjkldfhsah

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -3,8 +3,10 @@ import javax.swing.JOptionPane;
 import java.awt.Dimension;
 
 public class Runner {
+    public static String[] words = {"A","B","C","D","E"};
     public static void main(String[] args) {
         VirtualPet p = new VirtualPet();
+        
         p.face.clearMessage();
         p.exercise();
         p.rest(1000);
@@ -14,61 +16,30 @@ public class Runner {
         p.rest(1000);
         p.exercise();
         p.face.clearMessage();
+        int c = askQuestion("what do you choose", new String[] {"jfdklajflkadkj;fa", "me", "ok"}, p);
+        System.out.println(c);
+        p.exercise();
+        p.rest(1000);
+        p.exercise();
+        p.exercise();
+    }
 
-        p.exercise();
-        // //p.rest(2000);
-        
-        // JOptionPane.showMessageDialog(
-        //             new JFrame(),
-        //             "Hello!",
-        //             "???",
-        //             JOptionPane.PLAIN_MESSAGE
-        //             );
-        
-        // //VirtualPet p1 = new VirtualPet();
-        // //p.rest(2000);
-        // JOptionPane.showMessageDialog(
-        //             new JFrame(),
-        //             "Here is your pet!",
-        //             "???",
-        //             JOptionPane.PLAIN_MESSAGE
-        //             );
-        // VirtualPet p = new VirtualPet();
-        // p.face.setImage("normal");
-        // p.rest(1000);
-        // String[] options = {"Bob", "Kyle", "Other"};
-        // int selection = JOptionPane.showOptionDialog(new JFrame(), "Select your pet's name:", "Get started!", 
-        //                                                   0, 3, null, options, options[0]);
-        // String petName;
-        // if (selection == 0) {
-        //     petName = "Bob";
-        // }
-        // else if (selection == 1) {
-        //     petName = "Kyle";
-        // }
-        // else if (selection == 2) {
-        //     petName = (String)JOptionPane.showInputDialog(
-        //         new JFrame(),
-        //         "",
-        //         "Choose a name ",
-        //         JOptionPane.PLAIN_MESSAGE
-        //         );
-        // }
-        // else {
-        //     JOptionPane.showMessageDialog(
-        //             new JFrame(),
-        //             "bruh",
-        //             "bruh",
-        //             JOptionPane.WARNING_MESSAGE
-        //             );
-        //             petName = "";
-        // }
-        // //String petName = getResponse()
-        
-        // String[] s = {"make them exercise", "feed them", "put them to sleep...", "idk u ask me", "bruh"};
-        // int choice = chooseOptions(petName + " is tired", s);
-        // System.exit(1);
-        
+    public static int askQuestion(String q, String[] choices, VirtualPet p) {
+        p.face.newButtons(3);
+        for (int i = 0; i < choices.length; i++) {
+            p.face.setMessage(words[i] + ". " + choices[i]);
+        }
+        while (!p.face.buttonPressed) {
+            p.rest(1);
+        }
+        for (int i = 0; i < p.face.buttonStates.length; i++) {
+            if (p.face.buttonStates[i] == 1) {
+                p.face.clearMessage();
+                return i;
+            }
+        }
+        p.face.clearMessage();
+        return 0;
     }
 
     public String getResponse(String title, String question) {
